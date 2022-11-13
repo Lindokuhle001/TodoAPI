e7dc7186f416a650b5e189d881173612e7791513
lindokuhle shabalala <lindokuhle.shabalala@vcontractor.co.za>
2022-11-13 12:44:32 +0200

    server setup

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
+import express, {Request, Response} from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
 
+const port = process.env.NODE_PORT || 3000;
+const app = express();
+app.use(express.json());
+
+app.get('/',(req:Request, res: Response) => {
+    res.send("hi")
+})
+
+
+app.listen(port, () => {
+    console.log(`app listening on port: ${port}`);
+  });
